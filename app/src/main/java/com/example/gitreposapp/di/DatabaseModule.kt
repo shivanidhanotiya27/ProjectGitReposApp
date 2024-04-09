3cdfe38697b4b3e701f7e5775df17f4f3dd3be5d
Shivani Dhanotiya <shivani.dhanotiya@cognizant.com>
2024-04-09 14:46:07 +0530

    Initial commit for this Repo with basic structure

---
+++ app/src/main/java/com/example/gitreposapp/di/DatabaseModule.kt
@@ -0,0 +1,4 @@
+package com.example.gitreposapp.di
+
+class DatabaseModule {
+}
---
+++ app/src/main/java/com/example/gitreposapp/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gitreposapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gitreposapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
